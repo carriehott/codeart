1bfe24f3ce07a17e8dbaf7e4bf458eb471c6aee0
Carrie Hott <carriehott@gmail.com>
2018-11-15 17:31:07 -0800

    update

--- sketch.js
+++ sketch.js
@@ -1,15 +1,44 @@
+var img1;
+var img2;
+var img3;
+var img4;
+var img5;
+var xspeed = 10;
+var xpos = 0;
+
+
+function preload() {
+img1 = loadImage("images/static.jpg");
+img2 = loadImage("images/eye_1.png");
+img3 = loadImage("images/eye_2.png");
+img4 = loadImage("images/computer3.png");
+img5 = loadImage("images/mouth.png");
+}
+
 function setup() {
-  createCanvas(1800, 800);
-  noStroke();
+  createCanvas(1800, 1200);
 }
 function draw() {
   var x = mouseX;
   var y = mouseY;
   var ix = width - mouseX;  // Inverse X
   var iy = height - mouseY; // Inverse Y
-  background(126, 30, 20);
-  fill(255, 150, 200);
-  ellipse(x, height/2, y, y);
-  fill(0, 159, 30);
-  ellipse(ix, height/2, iy, iy);
+
+  tint(0, 153, 204, 126);
+  image(img1, 0, 0);
+
+  image(img2, x, 200);
+
+  image(img3, 800, 200, iy);
+
+  image(img5, x, iy, iy);
+
+  image(img4, xpos, 600);
+if (xpos > width) {
+  xspeed = -10;
+}
+if (xpos < 0) {
+  xspeed = 4;
+}
+xpos += xspeed;
 }
